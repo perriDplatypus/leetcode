d779af891524119e6439ccd9831c8ac4afad7ae2
Abhay <abhaytyagi2513Qgmail.com>
2025-07-13 00:19:01 +1000

    Solved more problems

--- 4. Median of two Sorted Arrays.js
+++ 4. Median of two Sorted Arrays.js
@@ -3,4 +3,34 @@
  * @param {number[]} nums2
  * @return {number}
  */
-var findMedianSortedArrays = function (nums1, nums2) {};
+var findMedianSortedArrays = function (nums1, nums2) {
+	let m = 0;
+	let n = 0;
+	let arr = [];
+
+	while (m < nums1.length && n < nums2.length) {
+		if (nums1[m] <= nums2[n]) {
+			arr.push(nums1[m]);
+			m++;
+		} else {
+			arr.push(nums2[n]);
+			n++;
+		}
+	}
+
+	while (m < nums1.length) {
+		arr.push(nums1[m]);
+		m++;
+	}
+
+	while (n < nums2.length) {
+		arr.push(nums2[n]);
+		n++;
+	}
+
+	if (arr.length % 2 === 0) {
+		return (arr[arr.length / 2 - 1] + arr[arr.length / 2]) / 2;
+	} else {
+		return arr[Math.floor(arr.length / 2)];
+	}
+};
